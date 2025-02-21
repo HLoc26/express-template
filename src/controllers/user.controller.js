d5038579f7caf76661baf5fbbb7d87616c29aac6
HLoc26 <dhl26052004@gmail.com>
2025-02-21 14:46:46 +0700

    refactor: remove console.log for cleaner terminal

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,7 +6,7 @@ export const getUser = handleRequest(async (req) => {
 	const token = req.cookies.jwt;
 	// console.log("token", token);
 	const user = decode(token);
-	console.log("HELLO", user);
+	// console.log("HELLO", user);
 	const ret = { userId: user.userId, usn: user.usn };
 	return ret;
 });
--- src/utils/handleRequest.js
+++ src/utils/handleRequest.js
@@ -2,7 +2,7 @@ export const handleRequest = (controller) => {
 	return async (req, res) => {
 		try {
 			const payload = await controller(req);
-			console.log(payload);
+			// console.log(payload);
 			res.json({
 				success: true,
 				payload,
